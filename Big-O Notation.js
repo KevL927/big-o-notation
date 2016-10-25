d0ba1c76b3ed673ebc125856917e460034468671
Michelle Nguyen <dr.mylann@gmail.com>
2016-10-25 13:44:35 -0500

    solved eq.3-4-5

--- Big-O Notation.js
+++ Big-O Notation.js
@@ -19,6 +19,7 @@ var findNthElement = function(array, n) {
     }
 };
 
+//----------------------------------------------------------------------
 
 
 
@@ -45,3 +46,64 @@ var findElements = function(array, toFind) {
 findElements(Array(160), [3,4]);
 
 console.log('findElements: ' + counter1 + ' findNthElement: ' + counter2 + ' pushCounter: ' + pushCounter + ' Equals to: ' + (counter1+counter2+pushCounter));
+
+
+//----------------------------------------------------------------------
+
+//O(1)- Constant time. How long it takes is independent of how many items. The holy grail of complexity.
+// Returns whether a number is odd or even
+var isOdd = function(number) {
+    var remainder = number % 2;
+    return remainder == 1;
+};
+
+//----------------------------------------------------------------------
+
+
+// Calculates the sample autocorrelation matrix (i.e. the matrix found by
+// multiplying every item in an array with every other item)
+
+//O(n^2) - Polynomial time. As n increases, things start to get slow pretty quickly.
+var counter1= 0;
+var arr = [1,2,3,4,5]
+var sampleAutocorrelationMatrix = function(array) {
+    var matrix = [];
+    for (var i=0; i<array.length; i++) {
+         counter1++;
+        var row = [];
+        for (var j=0; j<array.length; j++) {
+            counter1++;
+            row.push(array[i] * array[j]);
+        }
+        matrix.push(row);
+    }
+};
+
+sampleAutocorrelationMatrix(arr);
+console.log("Cost for this function with input size " + arr.length + " is " + counter1);
+
+
+//----------------------------------------------------------------------
+//O(n) - Linear time. Twice as many inputs will take roughly twice as much time.
+
+// Doubles every value in an array
+var doubleArray = function(array) {
+    var arr = [1, 2, 3, 4, 5];
+    for (var i=0; i<array.length; i++) {
+        array[i] = array[i] * 2;
+    }
+    return array;
+};
+
+doubleArray(arr);
+
+//----------------------------------------------------------------------
+
+// Calculates the nth triangle number
+// This one's a challenge! :)
+var triangleNumbers = function(n) {
+    if (n == 0 || n == 1) {
+        return n;
+    }
+    return triangleNumbers(n - 1) + triangleNumbers(n - 1) - triangleNumbers(n - 2) + 1;
+};
